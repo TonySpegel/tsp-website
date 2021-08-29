d5863c1738765cb693104c4ba79db190419a8178
Tony Spegel <TonySpegel@googlemail.com>
2021-08-29 23:10:04 +0200

    move code for blog posts into its own file

--- static/index.js
+++ static/index.js
@@ -1,15 +1,18 @@
 import { addPrefetchLink } from './js/addPrefetchLink.mjs';
-import './js/copyDirectLink.mjs';
-import './js/copyCode.mjs';
 import './js/intersectionObserver.mjs';
 
 /**
  * Prefetch Links
+ * ==============
  */
 
-// Get all page links and make an array out of it
-const pageLinks = [...document.querySelectorAll('a:not(.no-fetch)')];
 
+/**
+ * Get all page links and make an array out of it.
+ * Some links such as e-mail addresses and mobile numbers 
+ * should not be fetched - these are marked with `.no-fetch`.
+ */
+const pageLinks = [...document.querySelectorAll('a:not(.no-fetch)')];
 /**
  * Add an event listener for each link on mouseover
  * but do it only once to avoid unnecessary network requests
@@ -21,8 +24,14 @@ pageLinks.map((pageLink) => {
 });
 
 /**
+ * Misc.
+ * ==============
+ */
+
+
+/**
  * For iOS Safari there needs to be a touchstart listener in order to
- * apply :active styles. This can be acheived by using <body ontouchstart>.
+ * apply :active styles. This can be achieved by using <body ontouchstart>.
  * But this itself would cause Chrome to throw an error that this listener
  * isn't passive. To fix this the below code is needed.
  */
