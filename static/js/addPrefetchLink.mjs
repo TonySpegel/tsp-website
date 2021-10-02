0d665838e008f6ee601cedf3364eccde2ef2dc02
Tony Spegel <tonyspegel@googlemail.com>
2021-10-03 00:54:14 +0200

    delete files

--- static/js/addPrefetchLink.mjs
+++
@@ -1,8 +0,0 @@
-// Function to add a link to <head> which then gets prefetched
-export const addPrefetchLink = (event) => {
-    const link = document.createElement('link');
-    link.href = event.target.href;
-    link.rel = 'prefetch';
-
-    document.head.appendChild(link);
-};
