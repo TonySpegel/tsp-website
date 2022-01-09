f138330726de6fee7c35b56b4f9a84ea4a93d138
Tony Spegel <TonySpegel@googlemail.com>
2022-01-09 01:54:02 +0100

    v1.1.0: - use theme-switch 0.0.2 - use service worker w/ rollup

--- rollup.config.live.js
+++ rollup.config.live.js
@@ -1,9 +1,12 @@
 /**
- * Copyright © 2021 Tony Spegel
+ * Copyright © 2022 Tony Spegel
  */
 
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
+import summary from 'rollup-plugin-summary';
+import { generateSW } from 'rollup-plugin-workbox';
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'dist/index.js',
@@ -12,10 +15,26 @@ export default {
         format: 'es',
         sourcemap: false,
     },
+    preserveEntrySignatures: 'strict',
     plugins: [
         nodeResolve(),
-        replace({
-            preventAssignment: true,
+        /** Minify JS */
+        terser({ ecma: 2020, module: true, warnings: true }),
+        generateSW({
+            globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
+            navigateFallback: '/index.html',
+            // where to output the generated sw
+            swDest: path.join('dist', 'sw.js'),
+            // directory to match patterns against to be precached
+            globDirectory: path.join('dist'),
+            // cache any html js and css by default
+            globPatterns: ['**/*.{html,js,css,webmanifest}'],
+            skipWaiting: true,
+            clientsClaim: true,
+            runtimeCaching: [
+                { urlPattern: 'polyfills/*.js', handler: 'CacheFirst' },
+            ],
         }),
+        summary(),
     ],
 };
